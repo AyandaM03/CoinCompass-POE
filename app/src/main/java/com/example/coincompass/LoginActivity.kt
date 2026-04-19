dd63cb0f5ed496825cdac4d2cbb3dd2f20456fce
Alexis Maphosa <alexismaphosa3@gmail.com>
2026-04-19 13:33:55 +0200

    Updated LoginActivity logic and fixed

--- app/src/main/java/com/example/coincompass/LoginActivity.kt
+++ app/src/main/java/com/example/coincompass/LoginActivity.kt
@@ -1,20 +1,48 @@
 package com.example.coincompass
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.jvm.java
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //link UI components
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        val registerLink = findViewById<TextView>(R.id.registerLink)
+
+        // Set listener for login button click
+        loginBtn.setOnClickListener{
+            val userUsername = username.text.toString()
+            val userPassword = password.text.toString()
+            
+            // Validation: Check if any field is empty
+            if(userUsername.isEmpty()|| userPassword.isEmpty()){
+                Toast.makeText(this , "Please Fill all fields" , Toast.LENGTH_SHORT).show()
+
+            }else {
+            //go to dashborad/home page
+                val intent = Intent(this , DashboradActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
         }
-    }
+        //go to register screen
+       registerLink.setOnClickListener{
+           val intent = Intent(this , RegisterActivity :: class.java)
+           startActivity(intent)
+       }
+        }
+
+
 }
